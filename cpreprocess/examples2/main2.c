aeb34ecc9c3db09b4a75bc376bac4dc98c26d8b5
timeraider@gmx.at <timeraider@gmx.at>
2015-11-01 18:29:57 +0100

    added c-preprocessor grammar

---
+++ cpreprocess/examples2/main2.c
@@ -0,0 +1,12 @@
+/*
+ * leads to error during compilation!!!
+*/
+#include "bar.h"
+
+#include <stdio.h>
+
+int main(void) {
+	NUMBER number = 5;
+	printf("Number='%d'\n", number);
+	return 0;
+}
---
+++ cpreprocess/examples2/main4.c
@@ -0,0 +1,12 @@
+#define MYDEF
+#define OTHERDEF
+
+#include "bar.h"
+
+#include <stdio.h>
+
+int main(void) {
+	NUMBER number = 5;
+	printf("Number='%d'\n", number);
+	return 0;
+}
